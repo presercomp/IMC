5301f4f78e36db06eda673656d16caf86c566053
LAB1_PROF <desarrollo@presercomp.cl>
2018-09-27 16:34:21 -0300

    New Commit

---
+++ src/Clases/Helper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author LAB1_PC34
+ */
+public class Helper {
+    public static boolean isNumeric(String strNum) {
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
+    }
+}
